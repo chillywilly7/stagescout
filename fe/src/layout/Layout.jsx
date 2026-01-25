76f74350e31d38df050898336698d71c2d78f242
chillywilly7 <danniboy.gtz10@hotmail.com>
2026-01-25 00:06:03 -0600

    bug fixed

--- fe/src/layout/Layout.jsx
+++ fe/src/layout/Layout.jsx
@@ -162,6 +162,7 @@ export default function Layout({ children, currentPageName }) {
               Support
             </button>
             
+            
             {user ? (
               <>
                 <Link
